fbc52fafb2cf24ec74fc3bb97a861ab6ded3ff37
Caio-RB <caiorichate@proton.me>
2024-05-26 14:14:14 -0300

    Construindo: Migrando para vuejs

---
+++ componentes/navbar.js
@@ -0,0 +1,23 @@
+export default {
+	template:`
+	<div class="nav">
+  <input type="checkbox" id="nav-check">
+  <div class="nav-header">
+  </div>
+  <div class="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div class="nav-links">
+    <a href="#">Home</a>
+    <a href="#">Sobre</a>
+    <a href="#">Capitulos</a>
+    <a href="#">Apoiar</a>
+   <!--<a href="#">Link 5</a>-->
+  </div>
+</div>
+	`}
---
+++ main.js
@@ -0,0 +1,4 @@
+const app = Vue.createApp({});
+import navbar  from "./componentes/navbar.js"
+app.component("navbar", navbar);
+app.mount("#app");
