d27c956d3af98c43357469915a7d4131c5076da5
Caio-RB <caiorichate@proton.me>
2025-01-14 09:23:27 -0300

    .

--- src/componentes/support.js
+++ src/componentes/support.js
@@ -1,16 +1,26 @@
 export default{
 	template:`
 		<section id="support">
-			<span class="about__title">Apoie</span>
-			<div id="x" class="support__options social__container">
-				<span class="social__stamp">
-					<img src="src/assets/imagens/twitter_x.svg" class="social__image">
-				</span>
-				<span class="">
-					<button onclick="window.location.href='https://x.com/SK_BlackHeart';" class="social__button">Apoiar</button>
-				</span>
-				
-			</div>
-		</section>
+  <span class="about__title">Apoie</span>
+  
+  <div id="x" class="support__options social__container">
+    <span class="social__stamp">
+      <img src="src/assets/imagens/twitter_x.svg" class="social__image">
+    </span>
+    <span class="">
+      <button onclick="window.location.href='https://x.com/SK_BlackHeart';" class="social__button">Apoiar</button>
+    </span>
+  </div>
+  
+  <div id="x" class="support__options social__container">
+    <span class="social__stamp">
+      <img src="src/assets/imagens/monero.svg" class="social__image">
+    </span>
+    <span class="">
+      <button onclick="window.location.href='https://x.com/SK_BlackHeart';" class="social__button">Apoiar</button>
+    </span>
+  </div>
+</section>
+
 	`
 }
