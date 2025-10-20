883b15e3e2a9e570c4a5f0276a24332efa0c95c8
Caio-RB <caiorichate@proton.me>
2025-10-20 06:55:31 -0300

    Style: Organizando projeto

--- componentes/banner.js
+++
@@ -1,9 +0,0 @@
-export default{
-	template:`
-	<section id="banner">
-		<p class="section__title">Bem-vindo!</p>
-		<p class="section__text">Novos capitulos mensalmente</p>
-		<a v-bind:href="'#' + chapters" class="btn banner__btn">Ler Agora</a>
-	</section>
-	`
-}
